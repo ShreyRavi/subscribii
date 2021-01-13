af5d89b7d63d86b443179f6fea65b58152521650
ShreyRavi <shreyastallamraju@gmail.com>
2021-01-13 03:46:19 -0600

    added updated docs

--- .docz/.cache/async-requires.js
+++ .docz/.cache/async-requires.js
@@ -3,7 +3,6 @@ const preferDefault = m => (m && m.default) || m
 
 exports.components = {
   "component---privacy-policy-md": () => import("./../../../../PrivacyPolicy.md" /* webpackChunkName: "component---privacy-policy-md" */),
-  "component---readme-md": () => import("./../../../../README.md" /* webpackChunkName: "component---readme-md" */),
   "component---src-components-controls-mdx": () => import("./../../../../src/components/Controls.mdx" /* webpackChunkName: "component---src-components-controls-mdx" */),
   "component---src-components-header-mdx": () => import("./../../../../src/components/Header.mdx" /* webpackChunkName: "component---src-components-header-mdx" */),
   "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
--- .docz/.cache/sync-requires.js
+++ .docz/.cache/sync-requires.js
@@ -6,7 +6,6 @@ const preferDefault = m => (m && m.default) || m
 
 exports.components = {
   "component---privacy-policy-md": hot(preferDefault(require("/home/st/Code/subscribii/PrivacyPolicy.md"))),
-  "component---readme-md": hot(preferDefault(require("/home/st/Code/subscribii/README.md"))),
   "component---src-components-controls-mdx": hot(preferDefault(require("/home/st/Code/subscribii/src/components/Controls.mdx"))),
   "component---src-components-header-mdx": hot(preferDefault(require("/home/st/Code/subscribii/src/components/Header.mdx"))),
   "component---src-pages-404-js": hot(preferDefault(require("/home/st/Code/subscribii/.docz/src/pages/404.js"))),
