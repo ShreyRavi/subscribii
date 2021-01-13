5c86ab0e6e11620904f30394072c8d56159946ba
ShreyRavi <shreyastallamraju@gmail.com>
2021-01-13 03:14:32 -0600

    added docs

--- .docz/.cache/async-requires.js
+++ .docz/.cache/async-requires.js
@@ -4,6 +4,7 @@ const preferDefault = m => (m && m.default) || m
 exports.components = {
   "component---privacy-policy-md": () => import("./../../../../PrivacyPolicy.md" /* webpackChunkName: "component---privacy-policy-md" */),
   "component---readme-md": () => import("./../../../../README.md" /* webpackChunkName: "component---readme-md" */),
+  "component---src-components-controls-mdx": () => import("./../../../../src/components/Controls.mdx" /* webpackChunkName: "component---src-components-controls-mdx" */),
   "component---src-components-header-mdx": () => import("./../../../../src/components/Header.mdx" /* webpackChunkName: "component---src-components-header-mdx" */),
   "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
   "component---src-subscribii-app-mdx": () => import("./../../../../src/SubscribiiApp.mdx" /* webpackChunkName: "component---src-subscribii-app-mdx" */)
---
+++ .docz/.cache/caches/gatsby-plugin-mdx/mdx-scopes-dir/014e2bfc8f6fae881dea7438439fbe52.js
@@ -0,0 +1,14 @@
+import { Playground, Props } from 'docz';
+import Controls from "../../../../../src/components/Controls";
+import SubscriptionList from "../../../../../src/components/SubscriptionList";
+import { getCompareFunction, getAdjustedAmount } from "../../../../../src/util/util";
+import * as React from 'react';
+export default {
+  Playground,
+  Props,
+  Controls,
+  SubscriptionList,
+  getCompareFunction,
+  getAdjustedAmount,
+  React
+};
